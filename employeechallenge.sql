e8bf247f7ac413db01a8c1635eba73652eb1b332
Matt Eisenbart <matt.eisenbart@gmail.com>
2021-02-16 16:21:01 -0600

    Adds 2nd query

--- employeechallenge.sql
+++ employeechallenge.sql
@@ -1,7 +1,5 @@
-
-
 select 
-	employees.emp_no as "Employee Name", 
+	employees.emp_no as "Employee Num", 
 	last_name as "Last Name",
 	first_name as "First Name", 
 	sex as Sex,
@@ -12,4 +10,13 @@ from
 INNER JOIN salaries
 on salaries.emp_no = employees.emp_no
 ORDER BY employees.last_name
-	;
+;
+
+select
+	first_name as "First Name",
+	last_name as "Last Name",
+	hire_date as "Hire Date"	
+from
+	employees
+WHERE
+	EXTRACT(year FROM "hire_date") = 1986;
