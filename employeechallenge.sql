666cc508548a2b5721e0e6a54dc9a92d9d066472
Matt Eisenbart <matt.eisenbart@gmail.com>
2021-02-16 16:29:56 -0600

    Adds 3rd query

--- employeechallenge.sql
+++ employeechallenge.sql
@@ -20,3 +20,18 @@ from
 	employees
 WHERE
 	EXTRACT(year FROM "hire_date") = 1986;
+	
+select
+	dept_manager.dept_no as "Dept Num",
+	departments.dept_name as "Dept Name",
+	dept_manager.emp_no as "Employee Num",
+	employees.last_name as "Last Name",
+	employees.first_name as "First Name"	
+from
+	dept_manager
+INNER JOIN departments
+on departments.dept_no = dept_manager.dept_no
+INNER JOIN employees
+on employees.emp_no = dept_manager.emp_no
+ORDER BY employees.last_name
+;
